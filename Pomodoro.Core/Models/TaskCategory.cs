ddd371554cde360670852d7f64d719a6566f6ead
Федерягин Андрей <eniluck@mail.ru>
2022-04-12 20:44:25 +0500

    TaskCategory изменил core сущность

--- Pomodoro.Core/Models/TaskCategory.cs
+++ Pomodoro.Core/Models/TaskCategory.cs
@@ -2,7 +2,25 @@
 {
     public class TaskCategory
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+        public TaskCategory(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; }
+
+        public string? Name { get; }
+
+        public static (TaskCategory? Result, string[] Errors) Create(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                return (null, new[] { "Name cannot be null or whitespace." });
+            }
+
+            var newTaskCategory = new TaskCategory(0, name);
+            return (newTaskCategory, Array.Empty<string>());
+        }
     }
 }
