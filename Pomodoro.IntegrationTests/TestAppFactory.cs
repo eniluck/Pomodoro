0101d27ba2972e743992c195011b59a9fb398223
Федерягин Андрей <eniluck@mail.ru>
2022-04-19 00:01:52 +0500

    для тестов используем другую бд

---
+++ Pomodoro.IntegrationTests/TestAppFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Pomodoro.IntegrationTests
+{
+    public class TestAppFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.UseEnvironment("Tests");
+
+            base.ConfigureWebHost(builder);
+        }
+    }
+}
