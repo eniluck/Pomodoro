64b9b93253d0aa7cb1f7d575664ce2a562539aa6
Федерягин Андрей <eniluck@mail.ru>
2022-04-18 22:37:35 +0500

    попытка донастроить интеграционные тесты. fail =(

--- Pomodoro.IntegrationTests/TasksControllerTests.cs
+++ Pomodoro.IntegrationTests/TasksControllerTests.cs
@@ -1,12 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Pomodoro.Api.Contracts.Requests.Task;
 using Pomodoro.BL;
 using Pomodoro.Core;
+using Pomodoro.DAL.Postgres;
 using Xunit;
 
 namespace Pomodoro.IntegrationTests
@@ -20,11 +27,22 @@ namespace Pomodoro.IntegrationTests
             var app = new WebApplicationFactory<Program>();
             app.WithWebHostBuilder(builder =>
             {
-                /*builder.ConfigureTestServices(serviceCollection =>
+                builder.ConfigureTestServices(services =>
                 {
-                    serviceCollection.AddScoped
-                })*/
-            });
+                    /*
+                    //servicies.AddScoped
+                    var descriptor = services.SingleOrDefault(
+                        d => d.ServiceType == typeof(DbContextOptions<PomodoroDbContext>));
+
+                    services.Remove(descriptor);
+
+                    services.AddDbContext<PomodoroDbContext>(options =>
+                        options.UseNpgsql(
+                            connectionString,
+                            x => x.MigrationsAssembly("Pomodoro.DAL.Postgres")));*/
+                });
+            })
+                ;
 
             _client = app.CreateClient();
         }
@@ -46,7 +64,7 @@ namespace Pomodoro.IntegrationTests
                 CategoryId = null,
             };
 
-            var response = await _client.PostAsJsonAsync("api/tasks/CreateTask", newTask);
+            var response = await _client.PostAsJsonAsync("api/tasks", newTask);
 
             response.EnsureSuccessStatusCode();
         }
@@ -64,10 +82,31 @@ namespace Pomodoro.IntegrationTests
                 CategoryId = null,
             };
 
-            var response = await _client.PostAsJsonAsync("api/tasks/CreateTask", newTask);
+            var response = await _client.PostAsJsonAsync("api/tasks", newTask);
 
             Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
         }
 
+        [Fact]
+        public async Task UpdateTask_shouldReturnOK()
+        {
+            //how can i prepare for this scenario?
+
+            var id = 1;
+
+            var putTaskRequest = new PutTaskRequest()
+            {
+                Name = "some",
+                CategoryId = 1,
+                PomodoroEstimation = 1,
+                Status = Core.Models.TaskStatusModel.InList,
+            };
+
+            var response = await _client.PutAsJsonAsync($"api/tasks/{id}", putTaskRequest);
+
+            response.EnsureSuccessStatusCode();
+        }
     }
+
+
 }
