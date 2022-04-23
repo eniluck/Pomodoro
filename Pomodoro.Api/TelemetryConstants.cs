dd6f8400c189ae161d28b9b5ce2c70730872d067
Федерягин Андрей <eniluck@mail.ru>
2022-04-23 17:41:22 +0500

    jaeger +  opentelemetry

---
+++ Pomodoro.Api/TelemetryConstants.cs
@@ -0,0 +1,7 @@
+﻿public static class TelemetryConstants
+{
+    /// <summary>
+    /// The name of the <see cref="ActivitySource"/> that is going to produce our traces.
+    /// </summary>
+    public const string MyAppTraceSource = "Pomodoro.Api";
+}
