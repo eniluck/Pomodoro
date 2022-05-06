7d802d67cef021cbe7dc8c4d1519d14f81251ebd
Федерягин Андрей <eniluck@mail.ru>
2022-05-07 00:13:13 +0500

    шо та поднастроил.

--- Pomodoro.Api/Contracts/Requests/Task/CreateTaskRequest.cs
+++ Pomodoro.Api/Contracts/Requests/Task/CreateTaskRequest.cs
@@ -4,7 +4,6 @@ namespace Pomodoro.Api.Contracts.Requests.Task
 {
     public class CreateTaskRequest
     {
-        [Required]
         public string Name { get; set; } = string.Empty;
 
         public int? CategoryId { get; set; }
