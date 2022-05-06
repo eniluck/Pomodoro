9623ed28895348169a633b8ad86825770deaed9a
Федерягин Андрей <eniluck@mail.ru>
2022-05-07 00:13:13 +0500

    шо та поднастроил.

--- Pomodoro.Api/Contracts/Requests/Category/CreateCategoryRequest.cs
+++ Pomodoro.Api/Contracts/Requests/Category/CreateCategoryRequest.cs
@@ -4,7 +4,6 @@ namespace Pomodoro.Api.Contracts.Requests.Task
 {
     public class CreateCategoryRequest
     {
-        [Required]
         public string Name { get; set; } = string.Empty;
     }
 }
