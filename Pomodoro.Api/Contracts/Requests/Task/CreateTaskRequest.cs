fdafcff261baee6b3ca71137f820207e560998f3
Федерягин Андрей <eniluck@mail.ru>
2022-05-07 21:36:03 +0500

    добавил ограничение для длины строки в контрактах для API

--- Pomodoro.Api/Contracts/Requests/Task/CreateTaskRequest.cs
+++ Pomodoro.Api/Contracts/Requests/Task/CreateTaskRequest.cs
@@ -4,6 +4,8 @@ namespace Pomodoro.Api.Contracts.Requests.Task
 {
     public class CreateTaskRequest
     {
+        [Required]
+        [MaxLength(1024)]
         public string Name { get; set; } = string.Empty;
 
         public int? CategoryId { get; set; }
