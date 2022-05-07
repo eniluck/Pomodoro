9e794237b166b095a2e2b8622057f47facf91f16
Федерягин Андрей <eniluck@mail.ru>
2022-05-07 15:23:30 +0500

    Добавил fluent api описание для сущностей.
    Изменил размер текстовых полей. Добавил миграции.

---
+++ Pomodoro.DAL.Postgres/Configurations/TaskConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Pomodoro.DAL.Postgres.Entities;
+
+namespace Pomodoro.DAL.Postgres.Configurations;
+
+public class TaskConfiguration : IEntityTypeConfiguration<TaskEntity>
+{
+    public void Configure(EntityTypeBuilder<TaskEntity> builder)
+    {
+        builder.ToTable("Task");
+
+        builder.HasKey(b => b.Id);
+
+        // TODO: FK TaskCategory
+        builder.HasOne(b => b.Category)
+            .WithMany()
+            .HasForeignKey(b => b.CategoryId)
+            .HasConstraintName("FK_Task_TaskCategory_CategoryId");
+
+        builder.Property(b => b.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(b => b.Name)
+            .IsRequired()
+            .HasMaxLength(1024);
+
+        builder.Property(b => b.Status)
+            .IsRequired();
+    }
+}
--- Pomodoro.DAL.Postgres/Entities/TaskCategoryEntity.cs
+++ Pomodoro.DAL.Postgres/Entities/TaskCategoryEntity.cs
@@ -6,7 +6,6 @@ namespace Pomodoro.DAL.Postgres.Entities
     {
         public int Id { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
     }
 }
